03278ae55bd71d58ef3512afc135701e1342bc32
Thibaut Dubernet <thibaut.dubernet@ivt.baug.ethz.ch>
2017-10-09 13:00:01 +0200

    starter code for Exercise 3

---
+++ src/ch/ethz/ivt/abmt/exercise3/Exercise3_3.java
@@ -0,0 +1,15 @@
+package ch.ethz.ivt.abmt.exercise3;
+
+import java.io.IOException;
+
+public class Exercise3_3 {
+	public static void main( String[] args ) {
+		// write code that calls performIO() 30 times,
+		// and prints "i: result" when IO succeeds,
+		// with i the iteration number and result the output of perform IO.
+		// After printing, you should throw a RuntimeException if the result is negative
+		// If an IOException occurred, you should print a message stating so.
+		// If a RuntimeException was raised, you should print a message stating so
+
+	}
+}
---
+++ src/ch/ethz/ivt/abmt/exercise3/Exercise3_4.java
@@ -0,0 +1,14 @@
+package ch.ethz.ivt.abmt.exercise3;
+
+import java.io.IOException;
+
+public class Exercise3_4 {
+	public static void main( String[] args ) {
+		// write code that calls performIO(),
+		// and wraps the exception in an UncheckedIOException if it occurs
+		// Whatever happens, a message should be printed to the console
+		// If no exception was raised, an additional message should be printed
+		// to the console
+
+	}
+}
